799df94a155d927adf138aef0fe1d85a953f487c
dianekim94 <diane.da.kim@gmail.com>
2015-10-08 22:59:47 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+app.get('/nickname', function(request, response){
+    response.send('rich-pheasant');
 });
 
 app.listen(process.env.PORT || 4000);
